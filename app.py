0d830ea8a74fe78e09b6ef292ec79ddefab7ea0a
vikhanmuhammad <muhammadvikhanmuharram@gmail.com>
2024-05-16 17:44:53 +0700

    Versi Pickle
    Data dari pickle

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
 
 import warnings
+import pickle
 from flask import Flask, request, jsonify, render_template
 from models.calorie_model import train_model
 from models.obesity_model import train_obesity_model
@@ -22,9 +23,14 @@ def predict_calorie():
     gender = data['gender']
 
     bmi = calculate_bmi(height, weight, age, gender)
-    predicted_calories = knn_calorie_model.predict([[age, weight, height/100, calorie_label_encoder.transform([gender])[0]]])[0]
+    # predicted_calories = knn_calorie_model.predict([[age, weight, height/100, calorie_label_encoder.transform([gender])[0]]])[0]
+    with open('datasets/calories_classifier.pkl', 'rb') as file:
+        clf = pickle.load(file)
 
-    return jsonify({'predicted_calories': predicted_calories})
+    predicted_pkl = clf.predict([[age, weight, height/100, calorie_label_encoder.transform([gender])[0]]])[0]
+
+    return jsonify({'predicted_calories': predicted_pkl})
+    # return jsonify({'predicted_calories': predicted_calories})
 
 # Route for predicting obesity
 @app.route('/api/predict/obesity', methods=['POST'])
@@ -38,9 +44,13 @@ def predict_obesity():
 
     bmi = calculate_bmi(height, weight, age, gender)
     activity_category = map_activity_level(activity_level)
-    predicted_category = knn_obesity_model.predict([[bmi]])[0]
+    # predicted_category = knn_obesity_model.predict([[bmi]])[0]
+    with open('datasets/obesity_classifier.pkl', 'rb') as file:
+        clf = pickle.load(file)
+    
+    predicted_pkl = clf.predict([[bmi]])[0]
 
-    return jsonify({'predicted_category': predicted_category, 'bmi': bmi, 'activity_category': activity_category})
+    return jsonify({'predicted_category': predicted_pkl, 'bmi': bmi, 'activity_category': activity_category})
 
 # Run the Flask app
 if __name__ == "__main__":
